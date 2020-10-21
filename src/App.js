727b0c883f47a4e60b1ec51ca75d5b6d090e4e59
Artur Pabjańczyk <artur@pabjanczyk.pl>
2020-10-21 07:29:44 +0200

    Render canvas rectangle

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import './App.css';
 
+class CanvasExample extends React.Component {
+    render() {
+        return <canvas style={{border: "1px solid gray"}} width={300} height={300} />
+    }
+}
+
 function App() {
     return (
         <div className="App">
             <h1>Hello Canvas</h1>
+            <CanvasExample />
         </div>
     );
 }
