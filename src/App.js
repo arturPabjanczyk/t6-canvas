f0b3fe13a3ea7dae518bd36ebf926df8ea65617c
Artur Pabjańczyk <artur@pabjanczyk.pl>
2020-10-21 08:05:47 +0200

    Places rectangle filled with color in canvas area

--- src/App.js
+++ src/App.js
@@ -11,6 +11,11 @@ class CanvasExample extends React.Component {
         const ctx = this.canvas.current.getContext("2d");
         const {size} = this.props;
         ctx.clearRect(0, 0, size, size);
+        ctx.fillStyle = "rgba(0, 150, 0, 0.2)";
+        ctx.fillRect(30, 30, 150, 150);
+    }
+    componentDidMount() {
+        this.draw();
     }
 
     render() {
