85cf1966f11489956992908e75393d91fec00d78
Artur Pabjańczyk <artur@pabjanczyk.pl>
2020-10-21 07:47:56 +0200

    Accesses the canvas html element

--- src/App.js
+++ src/App.js
@@ -2,9 +2,13 @@ import React from 'react';
 import './App.css';
 
 class CanvasExample extends React.Component {
+    constructor(props) {
+        super(props);
+        this.canvas = React.createRef();
+    }
     render() {
         const {size} = this.props;
-        return <canvas style={{border: "1px solid gray"}} width={size} height={size} />
+        return <canvas ref={this.canvas} style={{border: "1px solid gray"}} width={size} height={size} />
     }
 }
 
