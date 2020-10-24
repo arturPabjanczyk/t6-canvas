d5872589fe144d93746770979d7fa9a172222db7
Artur Pabjańczyk <artur@pabjanczyk.pl>
2020-10-24 17:55:45 +0200

    Reformat

--- src/App.js
+++ src/App.js
@@ -2,78 +2,83 @@ import React from 'react';
 import './App.css';
 
 class CanvasExample extends React.Component {
-    constructor(props) {
-        super(props);
-        this.canvas = React.createRef();
-    }
+  constructor(props) {
+    super(props);
+    this.canvas = React.createRef();
+  }
 
-    draw() {
-        const ctx = this.canvas.current.getContext("2d");
-        const {size, rectanglesCount} = this.props;
-        ctx.clearRect(0, 0, size, size);
-        ctx.fillStyle = "rgba(0, 150, 0, 0.2)";
-        ctx.save();
-        ctx.translate(size/2,size/2);
-        const maxRectangleSize = size / Math.sqrt(2);
-        for (let i = 0; i < rectanglesCount; ++i) {
-            const ratio = maxRectangleSize * (i + 1);
-            const rectangleSize = ratio / rectanglesCount;
-            const angle = ratio;
-            ctx.save();
-            ctx.rotate(angle);
-            ctx.translate(-rectangleSize/2, -rectangleSize/2);
-            ctx.fillRect(0, 0, rectangleSize, rectangleSize);
-            ctx.restore();
-        }
-        ctx.restore();
-    }
+  componentDidMount() {
+    this.draw();
+  }
 
-    componentDidMount() {
-        this.draw();
-    }
+  componentDidUpdate() {
+    this.draw();
+  }
 
-    componentDidUpdate() {
-        this.draw();
+  draw() {
+    const ctx = this.canvas.current.getContext("2d");
+    const {size, rectanglesCount} = this.props;
+    ctx.clearRect(0, 0, size, size);
+    ctx.fillStyle = "rgba(0, 150, 0, 0.2)";
+    ctx.save();
+    ctx.translate(size / 2, size / 2);
+    const maxRectangleSize = size / Math.sqrt(2);
+    for (let i = 0; i < rectanglesCount; ++i) {
+      const ratio = maxRectangleSize * (i + 1);
+      const rectangleSize = ratio / rectanglesCount;
+      const angle = ratio;
+      ctx.save();
+      ctx.rotate(angle);
+      ctx.translate(-rectangleSize / 2, -rectangleSize / 2);
+      ctx.fillRect(0, 0, rectangleSize, rectangleSize);
+      ctx.restore();
     }
+    ctx.restore();
+  }
 
     render() {
-        const {size} = this.props;
-        return <canvas ref={this.canvas} style={{border: "1px solid gray"}} width={size} height={size}/>
-    }
+    const {size} = this.props;
+    return <canvas
+      ref={this.canvas}
+      style={{border: "1px solid gray"}}
+      width={size}
+      height={size}
+    />
+  }
 }
 
 class App extends React.Component {
-    state = {
-        rectangleCount: 10
-    }
+  state = {
+    rectangleCount: 10
+  }
 
-    render() {
-        return (
-            <div className="App">
-                <h1>Hello Canvas</h1>
-                <button
-                    onClick={() => {
-                        this.setState(prevState => ({
-                            rectangleCount: prevState.rectangleCount - 1
-                        }))
-                    }}
-                >
-                    -1
-                </button>
-                <button
-                    onClick={() => {
-                        this.setState(prevState => ({
-                            rectangleCount: prevState.rectangleCount + 1
-                        }))
-                    }}
-                >
-                    +1
-                </button>
-                <br/>
-                <CanvasExample size={300} rectanglesCount={this.state.rectangleCount}/>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello Canvas</h1>
+        <button
+          onClick={() => {
+            this.setState(prevState => ({
+              rectangleCount: prevState.rectangleCount - 1
+            }))
+          }}
+        >
+          -1
+        </button>
+        <button
+          onClick={() => {
+            this.setState(prevState => ({
+              rectangleCount: prevState.rectangleCount + 1
+            }))
+          }}
+        >
+          +1
+        </button>
+        <br/>
+        <CanvasExample size={300} rectanglesCount={this.state.rectangleCount}/>
+      </div>
+    );
+  }
 }
 
 export default App;
